588ee3d35be26fb261c001922576a03a232673af
csmizzle <chrissmith700@gmail.com>
2024-08-05 21:41:54 -0400

    adding rag endpoint

--- conductor_discord/bot.py
+++ conductor_discord/bot.py
@@ -5,6 +5,7 @@ import discord
 from conductor_discord.utils import (
     send_url_marketing_request,
     split_and_format_key_questions,
+    send_url_marketing_rag_request,
 )
 
 bot = discord.Bot()
@@ -57,6 +58,70 @@ async def research(
         thread_id=thread.id,
         style=report_style.upper(),
         key_questions=key_questions,
+    )
+    if request.ok:
+        await ctx.respond(
+            f"The team has started working! Check {thread_name} for updates!"
+        )
+    else:
+        await ctx.respond(
+            "Something went wrong. Please try again later. <@csmizzle> help!"
+        )
+
+
+@bot.slash_command(
+    name="research_v2", description="Submit a URL to kick off the market research team."
+)
+async def research_v2(
+    ctx: discord.ApplicationContext,
+    url: discord.Option(
+        discord.SlashCommandOptionType.string,
+        "URL to research, try to include http:// or https://.",
+        required=True,
+    ),  # type: ignore
+    title: discord.Option(
+        discord.SlashCommandOptionType.string,
+        "Title of the report",
+        required=True,
+    ),  # type: ignore
+    description: discord.Option(
+        discord.SlashCommandOptionType.string,
+        "Description of the report",
+        required=True,
+    ),  # type: ignore
+    report_style: discord.Option(
+        discord.SlashCommandOptionType.string,
+        "Report Style",
+        required=True,
+        choices=["Narrative", "Bulleted"],
+    ),  # type: ignore
+    report_tone: discord.Option(
+        discord.SlashCommandOptionType.string,
+        "Report Tone",
+        required=True,
+        choices=["Professional", "Informal", "Informational", "Persuasive", "Critical"],
+    ),  # type: ignore
+    report_pov: discord.Option(
+        discord.SlashCommandOptionType.string,
+        "Report Point of View",
+        required=True,
+        choices=["First Person", "Third Person"],
+    ),  # type: ignore
+) -> None:
+    # append https:// if not included
+    if not url.startswith("https://"):
+        url = f"https://{url}"
+    message = await ctx.send(f"Researching {url}...")
+    thread_name = f"{url} Research Thread"
+    thread = await message.create_thread(name=thread_name, auto_archive_duration=60)
+    request = send_url_marketing_rag_request(
+        company_url=url,
+        title=title,
+        description=description,
+        thread_id=thread.id,
+        style=report_style.upper(),
+        tone=report_tone.upper(),
+        point_of_view=report_pov.upper().replace(" ", "_"),
     )
     if request.ok:
         await ctx.respond(
--- conductor_discord/utils.py
+++ conductor_discord/utils.py
@@ -37,3 +37,47 @@ def send_url_marketing_request(
             os.getenv("CONDUCTOR_USERNAME"), os.getenv("CONDUCTOR_PASSWORD")
         ),
     )
+
+
+def send_url_marketing_rag_request(
+    company_url: str,
+    title: str,
+    description: str,
+    thread_id: str,
+    style: str,
+    tone: str,
+    point_of_view: str,
+) -> Response:
+    """
+    Sends a request to the marketing RAG (Red, Amber, Green) system for a URL.
+
+    Args:
+        company_url (str): The URL of the company.
+        title (str): The title of the request.
+        description (str): The description of the request.
+        thread_id (str): The ID of the thread associated with the request.
+        style (str): The style of the request.
+        tone (str): The tone of the request.
+        point_of_view (str): The point of view of the request.
+
+    Returns:
+        Response: The response from the marketing RAG system.
+    """
+    endpoint = os.getenv("CONDUCTOR_URL") + "/api/v1/discord/marketing/report/"
+    return requests.post(
+        url=endpoint,
+        json={
+            "url": company_url,
+            "title": title,
+            "description": description,
+            "thread_id": thread_id,
+            "proxy": False,
+            "cache": True,
+            "style": style,
+            "tone": tone,
+            "point_of_view": point_of_view,
+        },
+        auth=HTTPBasicAuth(
+            os.getenv("CONDUCTOR_USERNAME"), os.getenv("CONDUCTOR_PASSWORD")
+        ),
+    )
